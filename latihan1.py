7dcd9a5ae2745110a111f20c05e8c1fafb39b786
Rayhan Adam Aria <115473812+rayhanadam@users.noreply.github.com>
2022-10-23 11:16:21 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,4 @@
+#Menampilkan tulisan "Hello"
+print("Hello")
+#Menampilkan tulisan "Saya sedang belajar Python"
+print("Saya sedang belajar python")
---
+++ latihan3.py
@@ -0,0 +1,17 @@
+#input nilai variabel
+a = input("masukan nilai pertama: ")
+b = input("masukan nilai kedua: ")
+
+#cetak nilai variabel
+print("variabel a = ", a)
+print("variabel b = ", b)
+
+#cetak hasil kedua operasi variabel dengan string format
+print("Hasil Penggabungan {1} & {0} = ".format(a,b) + str(a)+str(b))
+
+#konversi nilai variabel 
+a = int(a);
+b = int(b);
+
+print("Hasil penjumlahan {1} + {0} = %d".format(a,b) %(a+b))
+print("Hasil pembagian {1} / {0} = %d".format(a,b) %(a/b))
